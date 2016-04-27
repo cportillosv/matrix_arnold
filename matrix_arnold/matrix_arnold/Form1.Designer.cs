69fb4619670e6844ca476d91e754b75e60ed8e18
cportillosv <cportillosv@outlook.com>
2016-04-26 22:52:20 -0600

    Primera pruebab

---
+++ matrix_arnold/matrix_arnold/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace matrix_arnold
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(460, 67);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(168, 55);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generar";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(136, 76);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 38);
+            this.txtNum1.TabIndex = 4;
+            this.txtNum1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNum1_KeyPress);
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(332, 76);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 38);
+            this.txtNum2.TabIndex = 5;
+            this.txtNum2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNum1_KeyPress);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(47, 205);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1043, 532);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 32);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "N1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 32);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "N2:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2004, 807);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Matriz";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ matrix_arnold/matrix_arnold/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace matrix_arnold
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            //Validando que no vaya vacio
+            if (string.IsNullOrEmpty(txtNum1.Text.Trim()))
+            {
+                MessageBox.Show("Digite un valor en N1");
+            }
+            else if (string.IsNullOrEmpty(txtNum2.Text.Trim()))
+            {
+                MessageBox.Show("Digite un valor en N2");
+            }
+            else
+            {
+                int num1 = Convert.ToInt32(txtNum1.Text);
+                int num2 = Convert.ToInt32(txtNum2.Text);
+                
+                int last=0;
+                //i= filas
+                for (int i = 0; i < num1; i++)
+                {
+                    //j = columnas
+                    for (int j = 0; j < num2; j++)
+                    {
+                        NumericUpDown numeric = new NumericUpDown();
+                        numeric.Name = string.Concat("numeric", i, j);
+                        numeric.Width = 50;
+                
+                        flowLayoutPanel1.Controls.Add(numeric);
+                        last = j;
+                    }
+                   
+                    //Obtenemos la ultima instancia, del NumericUpDown creado.
+                    NumericUpDown currentNumeric = this.Controls.Find(string.Concat("numeric", i, last), true).FirstOrDefault() as NumericUpDown;
+                    //Asignamos un salto de linea en el control flowLayoutControl
+                    flowLayoutPanel1.SetFlowBreak(currentNumeric,true);
+                }
+
+            }
+        }
+
+        private void txtNum1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Solamente para que permita numeros, si no cancela el evento
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
